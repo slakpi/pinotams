3987ec5bf08b95095d4edbf50a4430502bf3be5e
Randy Widell <randy@slakr.org>
2019-05-07 17:37:31 -0700

    main() boilerplate.

--- pinotams.c
+++ pinotams.c
@@ -1,4 +1,87 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include "config_helpers.h"
+
+static const char *shortArgs = "st";
+static const struct option longArgs[] = {
+  { "stand-alone", no_argument,       0, 's' },
+  { "test",        no_argument,       0, 't' },
+  { 0,             0,                 0,  0  }
+};
+
+static int run = 1;
+
+static void signalHandler(int _signo)
+{
+  switch (_signo)
+  {
+  case SIGINT:
+  case SIGTERM:
+  case SIGHUP:
+    run = 0;
+    break;
+  }
+}
+
 int main(int _argc, char* _argv[])
 {
+  pid_t pid, sid;
+  int c, test = 0, standAlone = 0;
+
+  while ((c = getopt_long(_argc, _argv, shortArgs, longArgs, 0)) != -1)
+  {
+    switch (c)
+    {
+    case 's':
+      standAlone = 1;
+      break;
+    case 't':
+      standAlone = 1;
+      test = 1;
+      break;
+    }
+  }
+
+  if (standAlone)
+  {
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
+    signal(SIGHUP, signalHandler);
+
+    /* TODO: Do work. */
+
+    return 0;
+  }
+
+  pid = fork();
+
+  if (pid < 0)
+    return -1;
+  if (pid > 0)
+    return 0;
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0)
+    return -1;
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  signal(SIGINT, signalHandler);
+  signal(SIGTERM, signalHandler);
+  signal(SIGHUP, signalHandler);
+
+  /* TODO: Do work. */
+
   return 0;
 }
