46a87f12ee7df0d27fc385d3af8cecc72a009779
Randy Widell <randy@slakr.org>
2019-05-24 21:11:46 -0700

    Tweak CMake script.

--- docmake_debug.sh
+++ docmake_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export CC=gcc-8
 export CXX=g++-8
-cmake -DCMAKE_INSTALL_PREFIX="/home/pi/Projects/pinotams" -DETC_PREFIX="./" -DVAR_PREFIX="./" -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_INSTALL_PREFIX=$(realpath ..) -DETC_PREFIX="./" -DVAR_PREFIX="./" -DCMAKE_BUILD_TYPE=Debug ..
