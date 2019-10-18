e63913a8240a5bcd3675e7fecdaab936c993d5ec
Randy Widell <randy@slakr.org>
2019-10-18 08:17:44 -0700

    Updated CMake scripts for GCC 9.

--- docmake_debug.sh
+++ docmake_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export CC=gcc-8
-export CXX=g++-8
+export CC=gcc-9
+export CXX=g++-9
 cmake -DCMAKE_INSTALL_PREFIX=$(realpath ..) -DETC_PREFIX="./" -DVAR_PREFIX="./" -DCMAKE_BUILD_TYPE=Debug ..
--- docmake_release.sh
+++ docmake_release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export CC=gcc-8
-export CXX=g++-8
+export CC=gcc-9
+export CXX=g++-9
 cmake -DCMAKE_INSTALL_PREFIX="/usr/local/pinotams" -DCMAKE_BUILD_TYPE=Release ..
 
