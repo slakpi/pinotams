d3fe9691d95161ca0d999e29fee202220325b12e
Randy Widell <randy@slakr.org>
2020-11-28 15:15:56 -0800

    Removed scripts.

--- docmake_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export CC=gcc-9
-export CXX=g++-9
-cmake -DCMAKE_INSTALL_PREFIX=$(realpath ..) -DETC_PREFIX="./" -DVAR_PREFIX="./" -DCMAKE_BUILD_TYPE=Debug ..
--- docmake_release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export CC=gcc-9
-export CXX=g++-9
-cmake -DCMAKE_INSTALL_PREFIX="/usr/local/pinotams" -DCMAKE_BUILD_TYPE=Release ..
-
